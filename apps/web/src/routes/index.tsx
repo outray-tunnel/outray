0ec2143e7f01214de012113ec2311d412c5bbc8e
akinloluwami <akinkunmioye42@gmail.com>
2025-12-27 21:50:35 +0100

    feat: update routing to navigate to organization slug in main route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -4,13 +4,21 @@ import { authClient } from "../lib/auth-client";
 export const Route = createFileRoute("/")({
   component: () => {
     const { data, isPending } = authClient.useSession();
+    const { data: organizations, isPending: isOrgsPending } =
+      authClient.useListOrganizations();
 
-    if (isPending) return <div></div>;
+    if (isPending || isOrgsPending) return <div></div>;
 
     if (!data?.session) {
       return <Navigate to="/login" replace />;
     }
 
-    return <Navigate to="/dash" replace />;
+    return (
+      <Navigate
+        to="/$orgSlug"
+        params={{ orgSlug: organizations?.[0]?.slug! }}
+        replace
+      />
+    );
   },
 });
