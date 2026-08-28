a2a9e0eac36f271c1468f89c24a9d23afbfb544d
Akinkunmi <akinkunmioye42@gmail.com>
2026-08-28 10:23:24 +0100

    Extract settings index redirect

--- apps/web/src/routes/$orgSlug/settings/index.tsx
+++ apps/web/src/routes/$orgSlug/settings/index.tsx
@@ -1,15 +1,18 @@
 import { createFileRoute, Navigate, useParams } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$orgSlug/settings/")({
-  component: () => {
-    const {orgSlug  } = useParams({from:"/$orgSlug/settings"})
-    return (
-      <Navigate
-        to="/$orgSlug/settings/profile"
-        params={{
-          orgSlug
-        }}
-      />
-    );
-  },
+  component: SettingsIndexRedirect,
 });
+
+function SettingsIndexRedirect() {
+  const { orgSlug } = useParams({ from: "/$orgSlug/settings" });
+
+  return (
+    <Navigate
+      to="/$orgSlug/settings/profile"
+      params={{
+        orgSlug,
+      }}
+    />
+  );
+}
