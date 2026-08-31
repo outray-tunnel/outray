ff961f13a169f86cf60df275b8c9e1f64efb98c1
Akinkunmi <akinkunmioye42@gmail.com>
2026-08-31 09:58:42 +0100

    Format secret vault detail route

--- apps/web/src/routes/$orgSlug/secrets/vaults_.$projectSlug.tsx
+++ apps/web/src/routes/$orgSlug/secrets/vaults_.$projectSlug.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { VaultPageView } from "./projects_.$projectSlug";
 
-export const Route = createFileRoute(
-  "/$orgSlug/secrets/vaults_/$projectSlug",
-)({
+export const Route = createFileRoute("/$orgSlug/secrets/vaults_/$projectSlug")({
   head: () => ({ meta: [{ title: "Vault - OutRay Secrets" }] }),
   component: VaultPage,
 });
